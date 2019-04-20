f40791ea9a7fe2e34606a8fe660930c9e9704807
Insung Jung <superisis@gmail.com>
2019-04-20 14:36:36 +0900

    데이터를 ArrayList에 모두 집어넣음

--- app/src/main/java/kr/insungjung/jikbanglistviewexam/MainActivity.java
+++ app/src/main/java/kr/insungjung/jikbanglistviewexam/MainActivity.java
@@ -4,11 +4,17 @@ import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import kr.insungjung.jikbanglistviewexam.databinding.ActivityMainBinding;
+import kr.insungjung.jikbanglistviewexam.datas.Room;
 
 public class MainActivity extends AppCompatActivity {
 
-    ActivityMainBinding act;
+    List<Room> roomList = new ArrayList<>(); // 리스트를 멤버변수로 생성
+
+   ActivityMainBinding act;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +23,17 @@ public class MainActivity extends AppCompatActivity {
 
         act.cleanRoomCountTxt.setText("32개");
 
+        fillRooms();
+
+    }
+
+    void fillRooms() {
+        roomList.add(new Room(500, 35, "서구 둔산동", 3, "타임월드 인근~~"));
+        roomList.add(new Room(500, 45, "동구 둔산동", 7, "갤러리아 백화점 인근"));
+        roomList.add(new Room(500, 35, "북구 둔산동", 20, "저렴"));
+        roomList.add(new Room(500, 35, "남구 둔산동", 2, "롯데월드"));
+        roomList.add(new Room(500, 40, "동구 둔산동", 127, "시청근처"));
+        roomList.add(new Room(500, 35, "서구 둔산동", -2, "타임스퀘어 인근"));
 
     }
 }
