75a2588ea8f050cce196b58ee7e522b5f526d10b
Insung Jung <superisis@gmail.com>
2019-04-20 14:03:13 +0900

    Initial commit

---
+++ app/src/main/java/kr/insungjung/jikbanglistviewexam/MainActivity.java
@@ -0,0 +1,20 @@
+package kr.insungjung.jikbanglistviewexam;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kr.insungjung.jikbanglistviewexam.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding act;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        act = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        act.cleanRoomCountTxt.setText("32개");
+    }
+}
