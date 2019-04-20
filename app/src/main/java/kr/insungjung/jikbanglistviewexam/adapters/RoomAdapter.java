c67109b356b6602037cc62dfd57c888946942e1f
Insung Jung <superisis@gmail.com>
2019-04-20 15:44:41 +0900

    1) 데이터 객체를 추출 2) 리스트아이템 XML <-> Adapter 클래스 자바파일 연결 3) 데이터에 들어있는 값을 적절한 자리에 출력

--- app/src/main/java/kr/insungjung/jikbanglistviewexam/adapters/RoomAdapter.java
+++ app/src/main/java/kr/insungjung/jikbanglistviewexam/adapters/RoomAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -32,6 +33,17 @@ public class RoomAdapter extends ArrayAdapter<Room> {
         if (row == null) {
             row = inf.inflate(R.layout.room_list_item, null);
         }
+
+        Room roomData = mList.get(position);
+
+//        그냥 findViewById 가 아니고, row.findViewById 인 것이 매우 중요!!!
+        TextView paymentTxt = row.findViewById(R.id.paymentTxt);
+        TextView locationAndFloorTxt = row.findViewById(R.id.locationAndFloorTxt);
+        TextView desriptionTxt = row.findViewById(R.id.descriptionTxt);
+
+        paymentTxt.setText(String.format("%d/%d", roomData.deposit, roomData.month_pay));
+        locationAndFloorTxt.setText(String.format("%s, %d층", roomData.location, roomData.floor));
+        desriptionTxt.setText(roomData.description);
 
         return row;
     }
--- app/src/main/java/kr/insungjung/jikbanglistviewexam/datas/Room.java
+++ app/src/main/java/kr/insungjung/jikbanglistviewexam/datas/Room.java
@@ -2,11 +2,11 @@ package kr.insungjung.jikbanglistviewexam.datas;
 
 public class Room {
 
-    int deposit; // 보증금
-    int month_pay; // 월세
-    String location; // 지역
-    int floor; // 층수 (만약 지하라면 음수값으로)
-    String description; // 설명
+    public int deposit; // 보증금
+    public int month_pay; // 월세
+    public String location; // 지역
+    public int floor; // 층수 (만약 지하라면 음수값으로)
+    public String description; // 설명
 
     public Room(int deposit, int month_pay, String location, int floor, String description) {
         this.deposit = deposit;
